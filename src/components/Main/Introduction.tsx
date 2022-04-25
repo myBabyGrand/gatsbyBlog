0ba88936932a2cb933001c70d50886beeae6d12e
mybabygrand <amuro_ray@kakao.com>
2022-04-25 22:46:44 +0900

    Chapter 3. real data to Main page  - thumnail, profile-image Lazy loading

--- src/components/Main/Introduction.tsx
+++ src/components/Main/Introduction.tsx
@@ -1,6 +1,11 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
+import { IGatsbyImageData } from 'gatsby-plugin-image'
 import ProfileImage from 'components/Main/ProfileImage'
+
+type IntroductionProps = {
+  profileImage: IGatsbyImageData
+}
 
 const Background = styled.div`
   width: 100%;
@@ -43,16 +48,18 @@ const Title = styled.div`
   }
 `
 
-const Introduction: FunctionComponent = function () {
+const Introduction: FunctionComponent<IntroductionProps> = function ({
+  profileImage,
+}) {
   return (
     <Background>
       <Wrapper>
-        <ProfileImage />
+        <ProfileImage profileImage={profileImage} />
 
         <div>
           <SubTitle>myBabyGrand</SubTitle>
           <Title>myBabyGrand</Title>
-        </div>
+          </div>
       </Wrapper>
     </Background>
   )
--- src/components/Main/ProfileImage.tsx
+++ src/components/Main/ProfileImage.tsx
@@ -1,24 +1,27 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
 
-// 자신이 원하는 프로필 이미지 링크로 설정해주세요.
-const PROFILE_IMAGE_LINK =
-  'https://phinf.pstatic.net/contact/20201211_32/1607664562109WYlvy_PNG/image.png?type=s160'
-
-const ProfileImageWrapper = styled.img`
-width: 120px;
-height: 120px;
-margin-bottom: 30px;
-border-radius: 50%;
-
-@media (max-width: 768px) {
-  width: 80px;
-  height: 80px;
+type ProfileImageProps = {
+  profileImage: IGatsbyImageData
 }
+
+const ProfileImageWrapper = styled(GatsbyImage)`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 50%;
+
+  @media (max-width: 768px) {
+    width: 80px;
+    height: 80px;
+  }
 `
 
-const ProfileImage: FunctionComponent = function () {
-  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+const ProfileImage: FunctionComponent<ProfileImageProps> = function ({
+  profileImage,
+}) {
+  return <ProfileImageWrapper image={profileImage} alt="Profile Image" />
 }
 
 export default ProfileImage
